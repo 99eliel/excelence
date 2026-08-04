ed5186a76e88553d88fd43071e8f819ab6298748
99eliel <131190282+99eliel@users.noreply.github.com>
2026-08-04 19:52:30 -0300

    Atualiza PWA para reativacao v59

--- pwa.js
+++ pwa.js
@@ -1,5 +1,5 @@
 (function () {
-  const APP_VERSION = window.EXCELLENCE_SYSTEM_VERSION || '20260801-55';
+  const APP_VERSION = window.EXCELLENCE_SYSTEM_VERSION || '20260804-59';
   const RELOAD_KEY = 'excellence-system-reloaded-version';
   const CHECK_INTERVAL = 60 * 1000;
 
