b008ae0d6a14c27d28b70f2221ee72b54bcd44f4
99eliel <131190282+99eliel@users.noreply.github.com>
2026-08-09 22:17:31 -0300

    Atualizar PWA para v67

--- pwa.js
+++ pwa.js
@@ -1,5 +1,5 @@
 (function () {
-  const APP_VERSION = window.EXCELLENCE_SYSTEM_VERSION || '20260809-66';
+  const APP_VERSION = window.EXCELLENCE_SYSTEM_VERSION || '20260809-67';
   const RELOAD_KEY = 'excellence-system-reloaded-version';
   const CHECK_INTERVAL = 60 * 1000;
 
