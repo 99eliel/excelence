1f159ae6611774c1967fe5365fc33dd315fb4fe8
99eliel <131190282+99eliel@users.noreply.github.com>
2026-08-09 21:40:34 -0300

    Atualizar PWA para v65

--- pwa.js
+++ pwa.js
@@ -1,5 +1,5 @@
 (function () {
-  const APP_VERSION = window.EXCELLENCE_SYSTEM_VERSION || '20260809-64';
+  const APP_VERSION = window.EXCELLENCE_SYSTEM_VERSION || '20260809-65';
   const RELOAD_KEY = 'excellence-system-reloaded-version';
   const CHECK_INTERVAL = 60 * 1000;
 
