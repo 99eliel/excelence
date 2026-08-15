e98c66cc08a1af2e964a070e8fe5a29e5281a88b
99eliel <131190282+99eliel@users.noreply.github.com>
2026-08-15 07:07:18 -0300

    chore: alinhar PWA com versão 72

--- pwa.js
+++ pwa.js
@@ -1,5 +1,5 @@
 (function () {
-  const APP_VERSION = window.EXCELLENCE_SYSTEM_VERSION || '20260815-71';
+  const APP_VERSION = window.EXCELLENCE_SYSTEM_VERSION || '20260815-72';
   const RELOAD_KEY = 'excellence-system-reloaded-version';
   const CHECK_INTERVAL = 60 * 1000;
 
