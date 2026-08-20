7bae4b5c645ad39e0143a7b77fef50d45158367a
99eliel <131190282+99eliel@users.noreply.github.com>
2026-08-20 11:00:38 -0300

    chore: atualizar PWA para v80

--- pwa.js
+++ pwa.js
@@ -1,5 +1,5 @@
 (function () {
-  const APP_VERSION = window.EXCELLENCE_SYSTEM_VERSION || '20260820-79';
+  const APP_VERSION = window.EXCELLENCE_SYSTEM_VERSION || '20260820-80';
   const RELOAD_KEY = 'excellence-system-reloaded-version';
   const CHECK_INTERVAL = 60 * 1000;
 
