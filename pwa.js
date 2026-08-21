26eaac1c98e113d92c73bfe35dd9fea73c464721
99eliel <131190282+99eliel@users.noreply.github.com>
2026-08-21 18:37:38 -0300

    chore: alinhar PWA com versão 90

--- pwa.js
+++ pwa.js
@@ -1,5 +1,5 @@
 (function () {
-  const APP_VERSION = window.EXCELLENCE_SYSTEM_VERSION || '20260821-89';
+  const APP_VERSION = window.EXCELLENCE_SYSTEM_VERSION || '20260821-90';
   const RELOAD_KEY = 'excellence-system-reloaded-version';
   const CHECK_INTERVAL = 60 * 1000;
 
