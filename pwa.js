c822bb82f6f0a6ad50015f6869348d10be4824db
99eliel <131190282+99eliel@users.noreply.github.com>
2026-09-08 21:05:56 -0300

    chore: atualizar PWA para v105

--- pwa.js
+++ pwa.js
@@ -1,5 +1,5 @@
 (function () {
-  const APP_VERSION = window.EXCELLENCE_SYSTEM_VERSION || '20260908-104';
+  const APP_VERSION = window.EXCELLENCE_SYSTEM_VERSION || '20260908-105';
   const RELOAD_KEY = 'excellence-system-reloaded-version';
   const CHECK_INTERVAL = 60 * 1000;
 
