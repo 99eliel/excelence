6c9d90dc780f4655e194f6c98bc51f6d21d34a8d
99eliel <131190282+99eliel@users.noreply.github.com>
2026-09-14 20:23:31 -0300

    chore: atualizar cache PWA para v107

--- pwa.js
+++ pwa.js
@@ -1,5 +1,5 @@
 (function () {
-  const APP_VERSION = window.EXCELLENCE_SYSTEM_VERSION || '20260914-106';
+  const APP_VERSION = window.EXCELLENCE_SYSTEM_VERSION || '20260914-107';
   const RELOAD_KEY = 'excellence-system-reloaded-version';
   const CHECK_INTERVAL = 60 * 1000;
 
